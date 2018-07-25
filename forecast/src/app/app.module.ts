23ccaaa93c452a1aaf8b1ebbce8c0697b75195fd
Nazar Ilchuk <n.ilchuk@ticketstravelnetwork.com>
2018-07-25 18:12:43 +0300

    added bootstrap

--- forecast/src/app/app.module.ts
+++ forecast/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule,
